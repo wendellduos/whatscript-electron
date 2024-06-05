38b5b9311920cf14875c01b9a7e5195c403b7bb3
wendel <wendellduos@hotmail.com>
2024-06-05 09:34:23 -0300

    feat: implemented Login with remote API (running locally)

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,40 +1,98 @@
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  const usernameEl = document.getElementById("username");
+  const passwordEl = document.getElementById("password");
+
   const form = {
     username: document.getElementById("username").value,
-    usernameEl: document.getElementById("username"),
     password: document.getElementById("password").value,
-    passwordEl: document.getElementById("password"),
   };
 
   if (!form.username || !form.password) {
-    popup("Por favor preencha todos os campos.", {
+    popup("Por favor, preencha todos os campos.", {
       duration: 4000,
-      type: "info",
     });
-  } else if (form.username === "admin" && form.password === "admin") {
-    popup("Login efetuado!", {
-      duration: 4000,
-      type: "success",
-    });
-
-    loginSection.style.opacity = "0";
-
-    setTimeout(() => {
-      loginSection.style.display = "none";
-    }, 700);
-
-    API.userLoggedIn();
-
-    form.usernameEl.value = "";
-    form.passwordEl.value = "";
   } else {
-    popup("Usuário ou senha incorretos.", {
-      duration: 4000,
-      type: "error",
-    });
+    fetch("http://localhost:5330/sign-in/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    })
+      .then(async (res) => {
+        if (res.status === 401 || res.status === 404) {
+          popup("Usuário ou Senha incorretos!", {
+            type: "error",
+            duration: 3000,
+          });
+        }
+
+        return res.json();
+      })
+      .then((data) => {
+        if (!data.message) {
+          console.log("nice, save this data: ", data);
+
+          // save data in localStorage
+
+          popup("Login efetuado!", {
+            duration: 4000,
+            type: "success",
+          });
+
+          loginSection.style.opacity = "0";
+
+          setTimeout(() => {
+            loginSection.style.display = "none";
+          }, 700);
+
+          API.userLoggedIn();
+
+          usernameEl.value = "";
+          passwordEl.value = "";
+        }
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
   }
+
+  // const form = {
+  //   username: document.getElementById("username").value,
+  //   usernameEl: document.getElementById("username"),
+  //   password: document.getElementById("password").value,
+  //   passwordEl: document.getElementById("password"),
+  // };
+
+  // if (!form.username || !form.password) {
+  //   popup("Por favor preencha todos os campos.", {
+  //     duration: 4000,
+  //     type: "info",
+  //   });
+  // } else if (form.username === "admin" && form.password === "admin") {
+  //   popup("Login efetuado!", {
+  //     duration: 4000,
+  //     type: "success",
+  //   });
+
+  //   loginSection.style.opacity = "0";
+
+  //   setTimeout(() => {
+  //     loginSection.style.display = "none";
+  //   }, 700);
+
+  //   API.userLoggedIn();
+
+  //   form.usernameEl.value = "";
+  //   form.passwordEl.value = "";
+  // } else {
+  //   popup("Usuário ou senha incorretos.", {
+  //     duration: 4000,
+  //     type: "error",
+  //   });
+  // }
 });
 
 logoutBtn.addEventListener("click", () => {
